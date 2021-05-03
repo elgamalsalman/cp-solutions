a788aa2961f440f92f069e6466cb554d89f00137
elgamalsalman <elgamalsalman@gmail.com>
2021-05-03 17:21:45 +0200

    Updated oj.uz JOI18_stove

--- oj_uz/oj_uz_Solutions/JOI18_stove/stove.cpp
+++ oj_uz/oj_uz_Solutions/JOI18_stove/stove.cpp
@@ -2,28 +2,26 @@
 
 using namespace std;
 
-#define MODVAL 998244353
-
-typedef long long ll;
-
-ll	n, m, k, ans;
-
-ll binExp(ll base, ll exponent) {
-	if (!exponent) return 1;
-	ll tmp = binExp(base, exponent / 2);
-	tmp = tmp * tmp % MODVAL;
-	if (exponent % 2) tmp = tmp * base % MODVAL;
-	return tmp;
-}
+vector<int> a;
+int	g[100050], n, k;
 
 int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 
-	cin >> n >> m >> k;
-	if (n == 1) ans = binExp(k, m);
-	else if (m == 1) ans = binExp(k, n);
-	else for (ll i = 1; i <= k; i++) ans = (ans + ((binExp(i, n) - binExp(i - 1, n) + MODVAL) * binExp(k - i + 1, m))) % MODVAL;
+	cin >> n >> k;
+	for (int i = 0; i < n; i++) cin >> g[i];
+	sort(g, g + n);
+
+	for (int i = 1; i < n; i++) {
+		a.push_back(g[i] - g[i - 1] - 1);
+	}
+	sort(a.begin(), a.end());
+
+	int ans = n;
+	for (int i = 0; i < n - k; i++) {
+		ans += a[i];
+	}
 
 	cout << ans << '\n';
 }
